504ad580412d2531d7a257994a0056f4eb4debcc
Mikhalchuk Grigoriy <grigoriymikhalchuk@hotmail.com>
2019-05-26 11:00:35 +0600

    statistics page

---
+++ lib/src/models/ui/statistics_model.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class StatisticsModel{
+  final IconData iconData;
+  final String text;
+
+  StatisticsModel({this.text, this.iconData});
+}
--- lib/src/ui/pages/statistics_page.dart
+++ lib/src/ui/pages/statistics_page.dart
@@ -1,13 +1,60 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import '../../models/ui/statistics_model.dart';
 
 class StatisticsPage extends StatefulWidget {
   _StatisticsPageState createState() => _StatisticsPageState();
 }
 
-class _StatisticsPageState extends State<StatisticsPage> {
+class _StatisticsPageState extends State<StatisticsPage>
+    with TickerProviderStateMixin {
+  List<StatisticsModel> items = [
+    StatisticsModel(text: "Reports", iconData: Icons.receipt),
+    StatisticsModel(text: "Income & Expenses", iconData: Icons.receipt),
+    StatisticsModel(text: "Categories", iconData: Icons.receipt),
+    StatisticsModel(text: "Balance", iconData: Icons.receipt),
+    StatisticsModel(text: "Labels", iconData: Icons.receipt),
+    StatisticsModel(text: "Records", iconData: Icons.receipt)
+  ];
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 6, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return GridView.builder(
+      itemCount: 6,
+      gridDelegate:
+          SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+      itemBuilder: (context, position) {
+        return Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8),
+          ),
+          elevation: 2.0,
+          child: InkWell(
+            borderRadius: BorderRadius.circular(8),
+            onTap: () {},
+            child: Container(
+              alignment: Alignment.center,
+              child: Text(
+                "${items[position].text}",
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
